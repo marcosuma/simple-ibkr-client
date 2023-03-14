fe1c98ebe0afa220b3fd119a31996a4a8655f425
Marco Suma <smumrc@gmail.com>
2023-03-14 08:24:55 +0800

    Fix warning

--- technical_indicators/rsi/rsi.py
+++ technical_indicators/rsi/rsi.py
@@ -8,14 +8,12 @@ class RSI:
 
         rsi_above_70 = np.where(df['RSI_14'] >= 70, True, False)
         rsi_below_30 = np.where(df['RSI_14'] <= 30, True, False)
-        df['RSI_30_ok'] = rsi_below_30
-        df['RSI_70_ok'] = rsi_above_70
 
         hist = 7
         for i in range(hist, len(df.index)-hist):
-            df['RSI_30_ok'][i] = True \
+            df.loc[i, 'RSI_30_ok'] = True \
                 if rsi_below_30[i - hist:i].sum() > 0 \
                 else False
-            df['RSI_70_ok'][i] = True \
+            df.loc[i, 'RSI_70_ok'] = True \
                 if rsi_above_70[i - hist:i].sum() > 0 \
                 else False
